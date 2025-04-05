030fdf00d02320b2a5887bdea0931af94ca5d345
andresnino@gmail.com <andresnino@gmail.com>
2025-04-05 09:25:26 +0000

    Edited register

--- client_code/register/__init__.py
+++ client_code/register/__init__.py
@@ -35,4 +35,17 @@ class register(registerTemplate):
     else:
       # Machine is not in database -- need to be registered
       anvil.server.call('register_machine', self.input_serial.text, self.dropdown_machine_type.selected_value)
+
+  def input_customer_change(self, **event_args):
+    """This method is called when the text in this text box is edited"""
+    query = self.input_customer.text.strip()
+
+    if len(query) > 1:
+      results = app_tables.stores.search(store=q.ilike(f"%{query}%"))
+      self.drop_down_customer.items = [(r['store'],r) for r in results]
+      self.drop_down_customer.visible = True
+    else:
+      self.drop_down_customer.items = []
+      self.drop_down_customer.visible = False
+    
       
