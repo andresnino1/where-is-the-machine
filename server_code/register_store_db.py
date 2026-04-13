1f1f45cca626abb13c121d270ff30a82f94f655a
andresnino@gmail.com <andresnino@gmail.com>
2026-04-13 04:58:43 +0000

    Edited register_store_db

--- server_code/register_store_db.py
+++ server_code/register_store_db.py
@@ -14,12 +14,15 @@ import anvil.server
 
 @anvil.server.callable
 def register_store(store_name, store_address, state, store_phone, store_email,  store_contact_person):
-  if is_store_in_db(store_name):
+  if not(is_store_in_db(store_name)):
+    print('store was not in db and now is register')
+  else:
+    print('store was already in db, so it was no register again')
     
   
-  app_tables.stores.add_row(store=store_name, store_address=store_address, state=state, store_phone=store_phone, store_email=store_email, contact_person=store_contact_person)
+  # app_tables.stores.add_row(store=store_name, store_address=store_address, state=state, store_phone=store_phone, store_email=store_email, contact_person=store_contact_person)
   # print('store register successfuly')
-  return('ok')
+
 
 
 
