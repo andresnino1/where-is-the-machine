a171f1c4a85afc4d0102f84a7a26af1b1f589c1e
andresnino@gmail.com <andresnino@gmail.com>
2026-04-22 06:53:52 +0000

    Created search_machine.Repairs_In_Repeating_Panel and search_machine.Machines_In_Repeating_Panel; Edited search_machine; Deleted search_machine.ItemTemplate1, search_machine.ItemTemplate2, search_machine.RowTemplate1, search_machine.RowTemplate2 and 1 more

--- client_code/search_machine/Machines_In_Repeating_Panel/__init__.py
+++ client_code/search_machine/Machines_In_Repeating_Panel/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import ItemTemplate2Template
+from ._anvil_designer import Machines_In_Repeating_PanelTemplate
 from anvil import *
 import anvil.server
 import anvil.tables as tables
@@ -6,9 +6,12 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 
 
-class ItemTemplate2(ItemTemplate2Template):
+class Machines_In_Repeating_Panel(Machines_In_Repeating_PanelTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+
+    self.serial.text = self.item["serial"]
+    self.type.text = self.item["type"]
 
     # Any code you write here will run before the form opens.
