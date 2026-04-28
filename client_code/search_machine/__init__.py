776aaa23d47828f5e4754230a5acffa38fa5f90f
andresnino@gmail.com <andresnino@gmail.com>
2026-04-28 01:44:06 +0000

    Edited search_machine.row_template_repairs, search_machine and search_machine.row_template_machines

--- client_code/search_machine/__init__.py
+++ client_code/search_machine/__init__.py
@@ -27,6 +27,26 @@ class search_machine(search_machineTemplate):
     self.repeating_panel_machines.visible=False
     self.repeating_panel_repairs.visible=False
 
+  @handle("input_store", "change")
+  def input_store_change(self, **event_args):
+    """This method is called when the text in this text box is edited"""
+    store_name = self.input_store.text.strip()
+  
+    if len(store_name) > 1:
+      self.search_store(store_name) # function search the store name in database
+
+  @handle("drop_down_store", "change")
+  def drop_down_store_change(self, **event_args):
+    store_name = self.drop_down_store.selected_value
+    if store_name:
+      # if there is a store in the database the register machine button is enabled
+      store_with_machine = app_tables.machines.search(store_link=store_name)
+      if store_with_machine:
+        self.repeating_panel_machines.items=store_with_machine
+      else:
+        print ('the store doesnt have devices registered')
+
+
 
 # =============== SEARCH SERIAL FUNCTION =====================
 
@@ -34,17 +54,14 @@ class search_machine(search_machineTemplate):
 
     machines_list = app_tables.machines.get(serial=serial_search) # search the serial in database
     
-    # machines_list return an object with the complete row information
-    # and this object is send to repeating panel machines, so when open
-    # the form RowTemplate1 the information will be recovered using self.item['name of column']
-   
     # IF serial DOESN'T EXIST
-    if machines_list == None:
+    if machines_list is None:
       self.info_card_2.visible=True
       self.info_card.visible=False
       self.message_1.text = "Machine is not in Data Base"
 
-    # If serial number exists, the machine_list is send to repeating_panel
+    # If serial number exists, the machine_list return an object with the complete row information
+    # andt this is send to repeating_panel
     # the data sent is now in row_template_machines and row_template_repairs
     # the data will be display in the repeating panels using self.item[<column name>]
     else:
@@ -53,20 +70,23 @@ class search_machine(search_machineTemplate):
       self.repeating_panel_repairs.visible=True
       self.repeating_panel_machines.items=[machines_list]
       self.repeating_panel_repairs.items=machines_list['repairs_link']
-      # self.repeating_panel_machines.items=["No Serial Number in DB"]
-      # IF serial exists, the fields are DISABLED, so the user can not register a duplicated
+     
 
+  # ========================= Search Store Function ===========================
 
+  def search_store(self, store_name, **event_args):
+    query_store = app_tables.stores.search(store=q.ilike(f"%{store_name}%"))
+    self.drop_down_store.items = [(r['store'],r) for r in query_store]
+    # self.drop_down_store.visible = True
 
-    #   query_store = app_tables.stores.search(store=q.ilike(f"%{store_name}%"))
-    # store_list = [r['store'] for r in query_store]
-    # self.store_name_repeating_panel.items = store_list
-    # self.data_grid_store_name.visible=True
+    # Check if there is the store in the database
+    if [(r['store'],r) for r in query_store]:
+      if len([(r['store'],r) for r in query_store]) ==1:
+        self.drop_down_store_change()
+    else:  
+      print('no store to show')
 
-    # # If there is NOT an store in de database the REGISTER STORE BUTTON IS ENABLED
-    # if store_list == []:
-    #   self.store_name_repeating_panel.items=['Store Is Not In Data Base']
-    #   self.data_grid_store_name.visible=True
+    # If there is an EXACT MATCH in the query the function dropdown_store_chage is trigger manualy
+    # To ensure the unique value in the list is selected.
+    
 
- 
- 
